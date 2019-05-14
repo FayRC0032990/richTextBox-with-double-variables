c308f267cfa92412955b09c61f01653688c29dad
rc0032990 <rc0032990@CRO-24-05>
2019-05-14 10:22:18 +0100

    Task 1

--- richTextBox with double variables/Form1.cs
+++ richTextBox with double variables/Form1.cs
@@ -15,6 +15,16 @@ namespace richTextBox_with_double_variables
         public Form1()
         {
             InitializeComponent();
+            double pi = 3.141592653589793;
+            double c = 299792458;
+            richTextBox1.AppendText(pi.ToString("N3") +"\n");
+            richTextBox1.AppendText(pi.ToString("N5") +"\n");
+            richTextBox1.AppendText(pi.ToString("N15") + "\n");
+            richTextBox1.AppendText(c.ToString("0") + "\n");
+            richTextBox1.AppendText(c.ToString("N0") + "\n");
+            
+
+
         }
     }
 }
